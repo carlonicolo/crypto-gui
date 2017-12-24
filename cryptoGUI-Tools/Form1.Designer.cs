7537562650860394988697f4dc1639b25081907f
Karlitos <carlo.nicolo@hotmail.com>
2017-12-24 17:30:45 +0100

    Completed the exchange function and the allCrypto function

--- cryptoGUI-Tools/Form1.Designer.cs
+++ cryptoGUI-Tools/Form1.Designer.cs
@@ -29,11 +29,11 @@
         private void InitializeComponent()
         {
             this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.lblResult = new System.Windows.Forms.Label();
             this.textQuantity = new System.Windows.Forms.TextBox();
             this.button6 = new System.Windows.Forms.Button();
             this.cmbFiat = new System.Windows.Forms.ComboBox();
             this.cmbCrypto = new System.Windows.Forms.ComboBox();
-            this.label8 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
@@ -58,10 +58,7 @@
             this.checkTRX = new System.Windows.Forms.CheckBox();
             this.checkBTC = new System.Windows.Forms.CheckBox();
             this.btnTopTen = new System.Windows.Forms.Button();
-            this.btnTopPercentageHour = new System.Windows.Forms.Button();
-            this.btnTopPercentageDay = new System.Windows.Forms.Button();
             this.btnCheckBoxSelection = new System.Windows.Forms.Button();
-            this.lblResult = new System.Windows.Forms.Label();
             this.groupBox4.SuspendLayout();
             this.groupBox5.SuspendLayout();
             this.groupBox1.SuspendLayout();
@@ -76,16 +73,28 @@
             this.groupBox4.Controls.Add(this.button6);
             this.groupBox4.Controls.Add(this.cmbFiat);
             this.groupBox4.Controls.Add(this.cmbCrypto);
-            this.groupBox4.Controls.Add(this.label8);
             this.groupBox4.Controls.Add(this.label7);
             this.groupBox4.Controls.Add(this.label6);
             this.groupBox4.Controls.Add(this.label5);
             this.groupBox4.Location = new System.Drawing.Point(12, 12);
             this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Size = new System.Drawing.Size(568, 80);
+            this.groupBox4.Size = new System.Drawing.Size(618, 80);
             this.groupBox4.TabIndex = 0;
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "Exchange tool";
+            // 
+            // lblResult
+            // 
+            this.lblResult.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblResult.AutoSize = true;
+            this.lblResult.BackColor = System.Drawing.SystemColors.Control;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.ForeColor = System.Drawing.Color.DarkRed;
+            this.lblResult.Location = new System.Drawing.Point(469, 42);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 17);
+            this.lblResult.TabIndex = 4;
             // 
             // textQuantity
             // 
@@ -110,7 +119,6 @@
             this.cmbFiat.Items.AddRange(new object[] {
             "EUR",
             "USD"});
-            this.cmbFiat.SelectedIndex = 0;
             this.cmbFiat.Location = new System.Drawing.Point(336, 40);
             this.cmbFiat.Name = "cmbFiat";
             this.cmbFiat.Size = new System.Drawing.Size(121, 21);
@@ -125,20 +133,10 @@
             "Ripple",
             "Cardano",
             "Tron"});
-            this.cmbCrypto.SelectedIndex = 0;
             this.cmbCrypto.Location = new System.Drawing.Point(22, 40);
             this.cmbCrypto.Name = "cmbCrypto";
             this.cmbCrypto.Size = new System.Drawing.Size(121, 21);
             this.cmbCrypto.TabIndex = 1;
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(496, 26);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(37, 13);
-            this.label8.TabIndex = 0;
-            this.label8.Text = "Result";
             // 
             // label7
             // 
@@ -175,7 +173,7 @@
             this.groupBox5.Controls.Add(this.txtDebugOutput);
             this.groupBox5.Location = new System.Drawing.Point(10, 284);
             this.groupBox5.Name = "groupBox5";
-            this.groupBox5.Size = new System.Drawing.Size(568, 178);
+            this.groupBox5.Size = new System.Drawing.Size(618, 178);
             this.groupBox5.TabIndex = 1;
             this.groupBox5.TabStop = false;
             this.groupBox5.Text = "Output Console";
@@ -188,12 +186,13 @@
             this.txtDebugOutput.Location = new System.Drawing.Point(7, 19);
             this.txtDebugOutput.Multiline = true;
             this.txtDebugOutput.Name = "txtDebugOutput";
-            this.txtDebugOutput.Size = new System.Drawing.Size(555, 153);
+            this.txtDebugOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtDebugOutput.Size = new System.Drawing.Size(605, 153);
             this.txtDebugOutput.TabIndex = 0;
             // 
             // cmdDeserialise2
             // 
-            this.cmdDeserialise2.Location = new System.Drawing.Point(126, 261);
+            this.cmdDeserialise2.Location = new System.Drawing.Point(215, 264);
             this.cmdDeserialise2.Name = "cmdDeserialise2";
             this.cmdDeserialise2.Size = new System.Drawing.Size(75, 23);
             this.cmdDeserialise2.TabIndex = 2;
@@ -203,6 +202,8 @@
             // 
             // cmdClear2
             // 
+            this.cmdClear2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
             this.cmdClear2.Location = new System.Drawing.Point(481, 462);
             this.cmdClear2.Name = "cmdClear2";
             this.cmdClear2.Size = new System.Drawing.Size(88, 23);
@@ -213,6 +214,9 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox1.Controls.Add(this.checkBTS);
             this.groupBox1.Controls.Add(this.checkZEC);
             this.groupBox1.Controls.Add(this.checkBCC);
@@ -230,7 +234,7 @@
             this.groupBox1.Controls.Add(this.checkBTC);
             this.groupBox1.Location = new System.Drawing.Point(12, 116);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(568, 92);
+            this.groupBox1.Size = new System.Drawing.Size(618, 92);
             this.groupBox1.TabIndex = 4;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Crypto selection tool";
@@ -399,33 +403,17 @@
             this.checkBTC.TabIndex = 0;
             this.checkBTC.Text = "BTC";
             this.checkBTC.UseVisualStyleBackColor = true;
+            this.checkBTC.CheckedChanged += new System.EventHandler(this.checkBTC_CheckedChanged);
             // 
             // btnTopTen
             // 
-            this.btnTopTen.Location = new System.Drawing.Point(207, 261);
+            this.btnTopTen.Location = new System.Drawing.Point(296, 264);
             this.btnTopTen.Name = "btnTopTen";
             this.btnTopTen.Size = new System.Drawing.Size(90, 23);
             this.btnTopTen.TabIndex = 5;
             this.btnTopTen.Text = "Top 10 Rank";
             this.btnTopTen.UseVisualStyleBackColor = true;
-            // 
-            // btnTopPercentageHour
-            // 
-            this.btnTopPercentageHour.Location = new System.Drawing.Point(303, 261);
-            this.btnTopPercentageHour.Name = "btnTopPercentageHour";
-            this.btnTopPercentageHour.Size = new System.Drawing.Size(75, 23);
-            this.btnTopPercentageHour.TabIndex = 6;
-            this.btnTopPercentageHour.Text = "Top % Hour";
-            this.btnTopPercentageHour.UseVisualStyleBackColor = true;
-            // 
-            // btnTopPercentageDay
-            // 
-            this.btnTopPercentageDay.Location = new System.Drawing.Point(384, 261);
-            this.btnTopPercentageDay.Name = "btnTopPercentageDay";
-            this.btnTopPercentageDay.Size = new System.Drawing.Size(75, 23);
-            this.btnTopPercentageDay.TabIndex = 6;
-            this.btnTopPercentageDay.Text = "Top % Day";
-            this.btnTopPercentageDay.UseVisualStyleBackColor = true;
+            this.btnTopTen.Click += new System.EventHandler(this.btnTopTen_Click);
             // 
             // btnCheckBoxSelection
             // 
@@ -436,27 +424,19 @@
             this.btnCheckBoxSelection.Text = "Go";
             this.btnCheckBoxSelection.UseVisualStyleBackColor = true;
             // 
-            // lblResult
-            // 
-            this.lblResult.AutoSize = true;
-            this.lblResult.Location = new System.Drawing.Point(497, 43);
-            this.lblResult.Name = "lblResult";
-            this.lblResult.Size = new System.Drawing.Size(35, 13);
-            this.lblResult.TabIndex = 4;
-            this.lblResult.Text = "label1";
-            // 
             // Form1
             // 
-            this.ClientSize = new System.Drawing.Size(592, 492);
+            this.ClientSize = new System.Drawing.Size(642, 492);
             this.Controls.Add(this.btnCheckBoxSelection);
-            this.Controls.Add(this.btnTopPercentageDay);
-            this.Controls.Add(this.btnTopPercentageHour);
             this.Controls.Add(this.btnTopTen);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.cmdClear2);
             this.Controls.Add(this.cmdDeserialise2);
             this.Controls.Add(this.groupBox5);
             this.Controls.Add(this.groupBox4);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
             this.Text = "Crypto GUI Tool";
             this.groupBox4.ResumeLayout(false);
@@ -477,7 +457,6 @@
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.ComboBox cmbFiat;
         private System.Windows.Forms.ComboBox cmbCrypto;
-        private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label5;
@@ -486,8 +465,6 @@
         private System.Windows.Forms.TextBox txtDebugOutput;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button btnTopTen;
-        private System.Windows.Forms.Button btnTopPercentageHour;
-        private System.Windows.Forms.Button btnTopPercentageDay;
         private System.Windows.Forms.CheckBox checkBTS;
         private System.Windows.Forms.CheckBox checkZEC;
         private System.Windows.Forms.CheckBox checkBCC;
--- cryptoGUI-Tools/Form1.cs
+++ cryptoGUI-Tools/Form1.cs
@@ -10,10 +10,18 @@ namespace cryptoGUI_Tools
 {
     public partial class Form1 : Form
     {
+        //Array string where to put selection checkbox values
+        private List<string> lst_crypto = new List<string>();
+
         public Form1()
         {
             InitializeComponent();
+            cmbCrypto.SelectedIndex = 0;
+            cmbFiat.SelectedIndex = 0;
+            textQuantity.Text = "0";
         }
+
+
         #region UI events
 
         private void cmdDeserialise2_Click(object sender, EventArgs e)
@@ -33,7 +41,39 @@ namespace cryptoGUI_Tools
             txtDebugOutput.Text = string.Empty;
         }
 
+        /*
+         * Button method for fetching json data from https://api.coinmarketcap.com
+         * 
+         * Inside this method we create the right URL for getting json data, putting as 
+         * crypto the crypto selected in the combobox. In this way we will retrieve the 
+         * corresponding json info related to the crypto value chosen in the combobox.
+         * 
+         * Example:
+         * string selectedCrypto = cmbCrypto.SelectedItem.ToString(); // Bitcoin
+         * string url = @"https://api.coinmarketcap.com/v1/ticker/" + selectedCrypto + "/?convert=EUR";
+         * url= https://api.coinmarketcap.com/v1/ticker/Bitcoin/?convert=EUR
+         * 
+         */
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //lblResult.Text = cmbCrypto.SelectedItem.ToString();
+            //float amountVal = (float)double.Parse(textQuantity.Text);
 
+            //Console.WriteLine("Value of amountVal: " + amountVal);
+            //Console.WriteLine("Type of amountVal: " + amountVal.GetType());
+
+            string selectedCrypto = cmbCrypto.SelectedItem.ToString();
+            string url = @"https://api.coinmarketcap.com/v1/ticker/" + selectedCrypto + "/?convert=EUR";
+
+            //string url = @"https://api.coinmarketcap.com/v1/ticker/?convert=EUR";
+        
+            //Using System.Net
+            var json = new WebClient().DownloadString(url);
+
+            //This function make extract the value and make the computation
+            exchange(json);
+
+        }
         #endregion
 
 
@@ -50,9 +90,32 @@ namespace cryptoGUI_Tools
                 //
                 var jCrypto = JsonConvert.DeserializeObject<List<jsonCrypto>>(strJSON);
 
-                debugOutput("Here's our JSON Object " + jCrypto.GetType());
-                debugOutput("This should be equals to bitcoin ---> " + jCrypto[0].id.ToString());
+                //debugOutput("Here's our JSON Object " + jCrypto.GetType());
+                //debugOutput("This should be equals to bitcoin ---> " + jCrypto[0].id.ToString());
                 //debugOutput("Here's the id: " + jCrypto.id);
+
+                // 
+                String s = "";
+                jCrypto.ForEach(x => {
+                    s += "*****************************\r\n";
+                    s += "Name: " + x.name + "\r\n";
+                    s += "Symbol: " + x.symbol + "\r\n";
+                    s += "Rank: " + x.rank + "\r\n";
+                    s += "€ price: " + x.price_eur + "\r\n";
+                    s += "$ price " + x.price_usd + "\r\n";
+                    s += "BTC price " + x.price_btc + "\r\n";
+                    s += "% 1h " + x.percent_change_1h + "\r\n";
+                    s += "% 24h " + x.percent_change_24h + "\r\n";
+                    s += "% 7d " + x.percent_change_7d + "\r\n";
+                    s += "*****************************\r\n";
+                });
+
+                Console.WriteLine(s);
+
+
+
+                debugOutput(s);
+
 
 
             }
@@ -85,23 +148,76 @@ namespace cryptoGUI_Tools
 
         }
 
+        #endregion
+
+
+        #region button methods
+
+        private void exchange(string strJSON)
+        {
+            var jCrypto = JsonConvert.DeserializeObject<List<jsonCrypto>>(strJSON);
+            //debugOutput("This should be equals to the selected crypto ---> " + jCrypto[0].id.ToString());
+            //debugOutput("This should be equals to the price_eur ---> " + jCrypto[0].price_eur);
+            //debugOutput("This should be equals to the price_usd ---> " + jCrypto[0].price_usd);
+            //debugOutput("This should be equqlas to the entire object jcrypto ---> " + jCrypto[0].ToString());
+
+            float amountVal = (float)double.Parse(textQuantity.Text);
+
+            float exchange_result_eur = amountVal * jCrypto[0].price_eur;
+            float exchange_result_usd = amountVal * jCrypto[0].price_usd;
+            Console.WriteLine("Value of exchange_result_eur: " + exchange_result_eur);
+            Console.WriteLine("Value of exchange_result_usd: " + exchange_result_usd);
+            lblResult.Text = String.Format("{0:€#,##0.0000;(€#,##0.0000);Zero}", exchange_result_eur);
+
+            debugOutput(amountVal + " " + jCrypto[0].symbol.ToString() + " = " + exchange_result_eur);
+        }
+
+
+
+
+        /*
+        * Button method for fetching json data of the top 10 crypto currency
+        * from https://api.coinmarketcap.com
+        * 
+        */
+        private void btnTopTen_Click(object sender, EventArgs e)
+        {
+            string url = @"https://api.coinmarketcap.com/v1/ticker/?limit=10";
+            
+            //Using System.Net
+            var json = new WebClient().DownloadString(url);
+
+            //This function make extract the value and make the computation
+            deserialiseJSON(json);
+        }
+
+
+
+
+
 
 
         #endregion
 
-        private void button6_Click(object sender, EventArgs e)
+        #region checkbox controll
+
+
+
+        #endregion
+
+        private void checkBTC_CheckedChanged(object sender, EventArgs e)
         {
-            lblResult.Text = cmbCrypto.SelectedItem.ToString();
-            float amountVal = (float)double.Parse(textQuantity.Text);
-            Console.WriteLine("Value of amountVal: " + amountVal);
-            Console.WriteLine("Type of amountVal: " + amountVal.GetType());
-            //string url = @"https://api.coinmarketcap.com/v1/ticker/?convert=EUR";
-
-            ////Using System.Net
-            //var json = new WebClient().DownloadString(url);
-
-            //Console.WriteLine("This is the json variable content: " + json);
-            //deserialiseJSON(json);
+            
+            if (checkBTC.Checked)
+            {
+                lst_crypto.Add("Bitcoin");
+                //Console.WriteLine("This is the state of checkBTC.CheckState: " + checkBTC.CheckState);
+                Console.WriteLine("Value of lst_crypto: " + lst_crypto[0]);
+            }
+            else
+            {
+                Console.WriteLine("This is the state of checkBTC.CheckState: " + checkBTC.CheckState);
+            }
         }
     }
 }
